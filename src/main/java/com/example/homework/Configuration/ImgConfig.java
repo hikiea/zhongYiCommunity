72b681f800853065a4cad3983e688e4d6d1f7266
hikiea <5524458+hikiea@user.noreply.gitee.com>
2020-04-02 15:15:38 +0800

    添加了简历

--- src/main/java/com/example/homework/Configuration/ImgConfig.java
+++ src/main/java/com/example/homework/Configuration/ImgConfig.java
@@ -10,8 +10,8 @@ public class ImgConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 本地服务器
-        registry.addResourceHandler("/picture/head/**").addResourceLocations("file:" + "F:\\IDEA\\Spring_Boot\\zhongYiCommunity\\src\\main\\resources\\static\\picture\\head\\");
+//        registry.addResourceHandler("/picture/head/**").addResourceLocations("file:" + "F:\\IDEA\\Spring_Boot\\zhongYiCommunity\\src\\main\\resources\\static\\picture\\head\\");
         // 阿里云服务器
-//        registry.addResourceHandler("/picture/head/**").addResourceLocations("file:" + "C:\\Users\\Administrator\\Desktop\\head\\");
+        registry.addResourceHandler("/picture/head/**").addResourceLocations("file:" + "C:\\Users\\Administrator\\Desktop\\head\\");
     }
 }
